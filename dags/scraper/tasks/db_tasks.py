866650e3538a29032cb9f3307cd41934109a8720
rene <rene.ruprecht.396@gmail.com>
2025-09-02 17:45:49 +0200

    test5

--- dags/scraper/tasks/db_tasks.py
+++ dags/scraper/tasks/db_tasks.py
@@ -4,12 +4,11 @@ import os
 from airflow.decorators import task
 from airflow.models import Variable
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from scraper.models.product import Product
 
 S3URL = Variable.get("s3_url")
 
 
-def insert_product(product: Product):
+def insert_product(product):
     today = datetime.date.today()
 
     hook = PostgresHook(postgres_conn_id="products")
@@ -33,21 +32,21 @@ def insert_product(product: Product):
             created_at = EXCLUDED.created_at;
     """,
         (
-            product.id,
-            product.name,
-            product.brand,
-            product.unit,
-            product.base_price_value,
-            product.base_price_unit,
-            product.market,
-            product.valid_from,
-            product.valid_to,
-            product.info,
+            product["id"],
+            product["name"],
+            product["brand"],
+            product["unit"],
+            product["base_price_value"],
+            product["base_price_unit"],
+            product["market"],
+            product["valid_from"],
+            product["valid_to"],
+            product["info"],
             today,
         ),
     )
 
-    for price in product.prices:
+    for price in product["prices"]:
         cur.execute(
             """
             INSERT INTO prices (product_id, price_type, amount, condition)
@@ -56,10 +55,10 @@ def insert_product(product: Product):
             SET amount = EXCLUDED.amount,
                 condition = EXCLUDED.condition;
         """,
-            (product.id, price.price_type, price.amount, price.condition),
+            (product["id"], price["price_type"], price["amount"], price["condition"]),
         )
 
-    for disc in product.discount_percents:
+    for disc in product["discount_percents"]:
         cur.execute(
             """
             INSERT INTO discounts (product_id, discount_type, amount, condition)
@@ -68,7 +67,7 @@ def insert_product(product: Product):
             SET amount = EXCLUDED.amount,
                 condition = EXCLUDED.condition;
         """,
-            (product.id, disc.discount_type, disc.amount, disc.condition),
+            (product["id"], disc["discount_type"], disc["amount"], disc["condition"]),
         )
 
     conn.commit()
